1f993fb2a7531fc88fa5f053be65e5d8f2dae638
111100001 <fqf1fqf@gmail.com>
2025-03-16 22:11:59 +0000

    f

--- my-tubeup-project/src/main.py
+++ my-tubeup-project/src/main.py
@@ -4,17 +4,19 @@ from tubeup.TubeUp import TubeUp
 
 def main():
     # Initialize the TubeUp instance
-    tubeup = TubeUp(verbose=True, dir_path='~/.tubeup')
-    tubeup.dir_path='~/tessst'
+    tubeup = TubeUp(verbose=True, dir_path='.')
 
     # Example URLs to download and upload
     urls = [
-        'https://www.youtube.com/watch?v=TSOg8tNFlqo',
+        'https://www.bilibili.com/video/BV15S4y1d77W?p=1',
         # Add more URLs as needed
     ]
 
     # Download and upload videos to archive.org
     tubeup.get_resource_basenames(urls)
+    
+    #tubeup.upload_ia(urls)
+    
        
 
 if __name__ == '__main__':
